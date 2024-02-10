a90ecb9757cdc016dd45366d91824e57dfe988ed
Vinicius Cunha <vinicius2010.cunha@hotmail.com>
2024-02-10 03:37:15 -0300

    fix small issues

--- src/app/components/timer/timer-control/timer-control.component.ts
+++ src/app/components/timer/timer-control/timer-control.component.ts
@@ -13,7 +13,7 @@ import { TimerWebSocket } from 'src/app/models/timerWebSocket';
 })
 export class TimerControlComponent {
   public chosedTimer: Timer = new Timer();
-  public timers: Timer[];
+  public timers: Timer[] = [];
   public started: boolean;
 
   @ViewChild(CdTimerComponent)
@@ -64,7 +64,7 @@ export class TimerControlComponent {
         next: (response) => {
           this.timers = response.object;
 
-          if (this.timers && this.timers.length > 0){
+          if (this.timers.length > 0){
             this.chosedTimer = this.timers[0];
             this.resetTimer();
           }
